21a75a72de4cfbbac2f9f62000fb1a7612fbe48b
Steffen Martinsen <steffen.martinsen@sparebank1.no>
2020-02-12 13:21:06 +0100

    Testing lerna

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
+import {spawnSync} from 'child_process'
 import * as core from '@actions/core'
-import {wait} from './wait'
+
+interface Package {
+  name: string
+  version: string
+  private: boolean
+  location: string
+}
+
+const getChangedPackages = (): Package[] => {
+  const changedPackages = spawnSync('./node_modules/lerna/cli.js', [
+    'changed',
+    '--json'
+  ]).stdout.toString()
+  return changedPackages.length > 0 ? JSON.parse(changedPackages) : []
+}
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  const stuff = getChangedPackages()
+  core.exportVariable('test', 'data')
+  core.exportVariable('changes', JSON.stringify(stuff))
+  core.debug(JSON.stringify(stuff))
 }
 
 run()
